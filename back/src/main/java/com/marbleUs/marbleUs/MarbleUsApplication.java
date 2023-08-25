65e104a3d398fa5a008be9413049d50eb7246629
SEB_BE_45_전우현 <tbvjdngus@gmail.com>
2023-08-25 17:20:36 +0900

    "Welcome"

--- back/src/main/java/com/marbleUs/marbleUs/MarbleUsApplication.java
+++ back/src/main/java/com/marbleUs/marbleUs/MarbleUsApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MarbleUsApplication {
 
 	public static void main(String[] args) {
+
+
 		SpringApplication.run(MarbleUsApplication.class, args);
 	}
 
