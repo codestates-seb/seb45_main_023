7d4a28da962d0df076d093ec1cf5d5c99159287d
SEB_BE_45_전우현 <tbvjdngus@gmail.com>
2023-08-25 17:17:13 +0900

    "Welcome"

---
+++ back/src/main/java/com/marbleUs/marbleUs/MarbleUsApplication.java
@@ -0,0 +1,13 @@
+package com.marbleUs.marbleUs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarbleUsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarbleUsApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/marbleUs/marbleUs/MarbleUsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marbleUs.marbleUs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarbleUsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
