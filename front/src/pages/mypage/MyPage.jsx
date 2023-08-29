75cb3a60c784aff2f948fb2c275a528f953d0be0
toystory23 <ghlduq@naver.com>
2023-08-30 00:17:20 +0900

    #16 마이페이지 수정기능 목업 디자인 구현완료

--- front/src/pages/mypage/MyPage.jsx
+++ front/src/pages/mypage/MyPage.jsx
@@ -1,7 +1,14 @@
+import { useState } from 'react';
 import LeftSidebar from '../../components/mypage/LeftSidebar';
 import RightSidebar from '../../components/mypage/RightSidebar';
 
 export default function MyPage() {
+  const [isEdit, setIsEdit] = useState(false);
+
+  const handleEdit = () => {
+    setIsEdit(!isEdit);
+  };
+  console.log(isEdit);
   const footer =
     'M A R B L E U S < < U S E R < < < < < < < < < < < < < < < < < < < < < < < < < < S E B 4 5 < < < 2 0 2 3 0 8 2 4 2 0 2 3 0 9 2 2 < < < A L L R I G H T S R E S E R V E D';
   return (
@@ -19,7 +26,9 @@ export default function MyPage() {
           </div>
           <div className="flex justify-center bg-white h-[3.75rem] pb-2">
             <button className=" border-black border-1 px-2 mx-4 w-40">로그아웃</button>
-            <button className=" border-black border-1 px-2 mx-4 w-40">개인정보수정</button>
+            <button className=" border-black border-1 px-2 mx-4 w-40" onClick={handleEdit}>
+              {isEdit ? '수정완료' : '개인정보수정'}
+            </button>
           </div>
         </div>
         <div className="flex flex-col justify-between border-2 w-[50rem] h-[27.5rem] rounded-b-3xl bg-white">
@@ -31,22 +40,37 @@ export default function MyPage() {
             <div className="flex h-[17rem] bg-white">
               <div className=" border-1 border-black w-[12.5rem] bg-white">이미지자리</div>
               <div className="flex flex-col justify-between ml-10 bg-white">
-                <div>
+                <div className='flex flex-col'>
+                  <label className="bg-white" htmlFor='nickname'>닉네임/nickname</label>
+                  {isEdit ? (
+                    <input type="text" placeholder="변경할 닉네임" className="bg-white w-[12rem]" id='nickname'></input>
+                  ) : (
+                    <div className="font-bold bg-white">김코딩코딩</div>
+                  )}
+                </div>
+                <div className='flex flex-col'>
+                  <label className="bg-white" htmlFor='birth'>생년월일/Date of birth</label>
+                  {isEdit ? (
+                    <input type="date" className="bg-white w-[12rem]" id='birth'></input>
+                  ) : (
+                    <div className="font-bold bg-white">01 JAN 2000</div>
+                  )}
+                </div>
+                <div className='flex flex-col'>
+                  <label className="bg-white" htmlFor='nation'>국적/nationality</label>
+                  {isEdit ? (
+                    <input type="text" placeholder="국적을 적어주세요" className="bg-white w-[12rem]" id='nation'></input>
+                  ) : (
+                    <div className="font-bold bg-white">대한민국</div>
+                  )}
+                </div>
+                {isEdit?<div className='flex flex-col'>
+                  <label className="bg-white" htmlFor='pw'>비밀번호 변경</label>
+                  <input type='password' id='pw' className='bg-white'></input>
+                </div>:<div>
                   <div className="bg-white">메일/E-mail</div>
                   <div className="font-bold bg-white">user@example.com</div>
-                </div>
-                <div>
-                  <div className="bg-white">닉네임/nickname</div>
-                  <div className="font-bold bg-white">김코딩코딩</div>
-                </div>
-                <div>
-                  <div className="bg-white">생년월일/Date of birth</div>
-                  <div className="font-bold bg-white">01 JAN 2000</div>
-                </div>
-                <div>
-                  <div className="bg-white">국적/nationality</div>
-                  <div className="font-bold bg-white">대한민국</div>
-                </div>
+                </div>}
               </div>
             </div>
             <div className="flex whitespace-pre-line bg-white">{footer}</div>
