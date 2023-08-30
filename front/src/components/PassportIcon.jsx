a7379b03e1ad3f338f70970b8beb0d5f2daca14b
3kiein <daisy.airdrop_08@icloud.com>
2023-08-30 11:20:25 +0900

    [fix] #18 link 위치 변경

--- front/src/components/PassportIcon.jsx
+++ front/src/components/PassportIcon.jsx
@@ -9,39 +9,57 @@ export default function PassportIcon() {
 
 	return (
 		<div className="fixed bottom-10 right-10 cursor-pointer">
-			<section
-				className="flex w-34 h-69 p-6 flex-col justify-center items-end gap-3 rounded-r-1 shadow-xl"
-				style={{
-					color: "#EFF0ED",
-					backgroundColor: "#293676",
-					borderRadius: "0px 10px 10px 0px",
-					boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.5)",
-				}}
-			>
-				<img src={PassportImage} alt="passport logo" className="w-12 h-15 " />
-				<div className="flex flex-col justify-center items-end gap-2">
-					{isLoggedIn ? (
-						<Link to="/mypage">
+			{isLoggedIn ? (
+				<Link to="/mypage">
+					<section
+						className="flex w-34 h-69 p-6 flex-col justify-center items-end gap-3 rounded-r-1 shadow-xl"
+						style={{
+							color: "#EFF0ED",
+							backgroundColor: "#293676",
+							borderRadius: "0px 10px 10px 0px",
+							boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.5)",
+						}}
+					>
+						<img
+							src={PassportImage}
+							alt="passport logo"
+							className="w-12 h-15"
+						/>
+						<div className="flex flex-col justify-center items-end gap-2">
 							<div className="text-xs font-normal text-right">
 								<p>마이페이지</p>
 								<p>MY PAGE</p>
 							</div>
-						</Link>
-					) : (
-						<Link to="/login">
+						</div>
+						<i className="fa-solid fa-house-user ml-auto w-4 h-4 mr-0.5"></i>
+					</section>
+				</Link>
+			) : (
+				<Link to="/login">
+					<section
+						className="flex w-34 h-69 p-6 flex-col justify-center items-end gap-3 rounded-r-1 shadow-xl"
+						style={{
+							color: "#EFF0ED",
+							backgroundColor: "#293676",
+							borderRadius: "0px 10px 10px 0px",
+							boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.5)",
+						}}
+					>
+						<img
+							src={PassportImage}
+							alt="passport logo"
+							className="w-12 h-15"
+						/>
+						<div className="flex flex-col justify-center items-end gap-2">
 							<div className="text-xs font-normal text-right">
 								<p>로그인 / 회원가입</p>
 								<p>LOGIN / SIGN</p>
 							</div>
-						</Link>
-					)}
-				</div>
-				{isLoggedIn ? (
-					<i className="fa-solid fa-house-user ml-auto w-4 h-4 mr-0.5"></i>
-				) : (
-					<i className="fa-solid fa-right-to-bracket ml-auto w-4 h-4 mr-0.5"></i>
-				)}
-			</section>
+						</div>
+						<i className="fa-solid fa-right-to-bracket ml-auto w-4 h-4 mr-0.5"></i>
+					</section>
+				</Link>
+			)}
 		</div>
 	);
 }
