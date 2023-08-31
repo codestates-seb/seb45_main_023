6f76e6460fe4d1d522e834d57aecb34a95c2389f
toystory23 <ghlduq@naver.com>
2023-08-31 22:11:23 +0900

    #34 사이드바 CSS 수정 및 사용자 액션변환 추가

--- front/src/components/mypage/LeftSidebar.jsx
+++ front/src/components/mypage/LeftSidebar.jsx
@@ -1,33 +1,39 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faMap } from '@fortawesome/free-regular-svg-icons';
 import { faBook, faStamp, faBookBookmark } from '@fortawesome/free-solid-svg-icons';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom'; // Changed import
 import { RouteConst } from '../../interface/RouteConst';
+import { useRecoilState } from 'recoil';
+import { Routes } from '../../recoil/mypage';
 
 export default function LeftSidebar() {
+  const [routes, setRoutes] = useRecoilState(Routes);
+  const handleRoute = (route) => {
+    setRoutes(route);
+  };
   return (
     <div className="relative">
       <div className="flex flex-col justify-between w-[10rem] h-[10rem] absolute bottom-10 left-[-10rem]">
-        <Link to={RouteConst.mypage} className="bg-green-600 shadow-lg">
+        <NavLink to={RouteConst.mypage} activeClassName="active" className="bg-green-600 shadow-lg" onClick={()=>handleRoute(RouteConst.mypage)}>
           <FontAwesomeIcon icon={faUser} style={{ color: '#000000' }} className="bg-green-600 pl-4" />
           <span className="ml-2 bg-green-600 text-white">프로필</span>
-        </Link>
-        <Link to={RouteConst.myblog} className=" bg-yellow-500 shadow-lg">
+        </NavLink>
+        <NavLink to={RouteConst.myblog} activeClassName="active" className="bg-yellow-500 shadow-lg" onClick={()=>handleRoute(RouteConst.myblog)}>
           <FontAwesomeIcon icon={faBook} style={{ color: '#000000' }} className="bg-yellow-500 pl-4" />
           <span className="ml-2 bg-yellow-500 text-white">작성글 목록</span>
-        </Link>
-        <Link to={RouteConst.mystamp} className=" bg-cyan-700 shadow-lg">
+        </NavLink>
+        <NavLink to={RouteConst.mystamp} activeClassName="active" className="bg-cyan-700 shadow-lg" onClick={()=>handleRoute(RouteConst.mystamp)}>
           <FontAwesomeIcon icon={faStamp} style={{ color: '#000000' }} className="bg-cyan-700 pl-4" />
           <span className="ml-2 bg-cyan-700 text-white">스탬프</span>
-        </Link>
-        <Link to={RouteConst.mybookmark} className=" bg-red-600 shadow-lg">
+        </NavLink>
+        <NavLink to={RouteConst.mybookmark} activeClassName="active" className="bg-red-600 shadow-lg" onClick={()=>handleRoute(RouteConst.mybookmark)}>
           <FontAwesomeIcon icon={faBookBookmark} style={{ color: '#000000' }} className="bg-red-600 pl-4" />
           <span className="ml-2 bg-red-600 text-white">북마크</span>
-        </Link>
-        <Link to={RouteConst.mymission} className=" bg-purple-600 shadow-lg">
+        </NavLink>
+        <NavLink to={RouteConst.mymission} activeClassName="active" className="bg-purple-600 shadow-lg" onClick={()=>handleRoute(RouteConst.mymission)}>
           <FontAwesomeIcon icon={faMap} style={{ color: '#000000' }} className="bg-purple-600 pl-4" />
           <span className="ml-2 bg-purple-600 text-white">미션</span>
-        </Link>
+        </NavLink>
       </div>
     </div>
   );
--- front/src/components/mypage/RightSidebar.jsx
+++ front/src/components/mypage/RightSidebar.jsx
@@ -6,28 +6,31 @@ import { faBookBookmark } from '@fortawesome/free-solid-svg-icons';
 import { faMap } from '@fortawesome/free-regular-svg-icons';
 import { Link } from 'react-router-dom';
 import { RouteConst } from '../../interface/RouteConst';
+import { useRecoilValue } from 'recoil';
+import { Routes } from '../../recoil/mypage';
 
 export default function RightSidebar() {
+  const route = useRecoilValue(Routes);
   return (
     <div className="relative">
-      <div className="flex flex-col justify-between w-[8rem] h-[10rem] absolute top-20 left-0">
-        <Link to={RouteConst.mypage} className="bg-green-600">
+      <div className="flex flex-col justify-between w-[8rem] absolute top-20 left-0">
+        <Link to={RouteConst.mypage} className={`bg-green-600 ${route !== RouteConst.mypage ? 'hidden' : ''}`}>
           <FontAwesomeIcon icon={faUser} style={{ color: '#000000' }} className="bg-green-600 pl-4" />
           <span className="ml-2 bg-green-600 text-white">프로필</span>
         </Link>
-        <Link to={RouteConst.myblog} className=" bg-yellow-500 ">
+        <Link to={RouteConst.myblog} className={`bg-yellow-500 ${route !== RouteConst.myblog ? 'hidden' : ''}`}>
           <FontAwesomeIcon icon={faBook} style={{ color: '#000000' }} className="bg-yellow-500 pl-4" />
           <span className="ml-2 bg-yellow-500 text-white">작성글 목록</span>
         </Link>
-        <Link to={RouteConst.mystamp} className=" bg-cyan-700">
+        <Link to={RouteConst.mystamp} className={`bg-cyan-700 ${route !== RouteConst.mystamp ? 'hidden' : ''}`}>
           <FontAwesomeIcon icon={faStamp} style={{ color: '#000000' }} className="bg-cyan-700 pl-4" />
           <span className="ml-2 bg-cyan-700 text-white">스탬프</span>
         </Link>
-        <Link to={RouteConst.mybookmark} className=" bg-red-600">
+        <Link to={RouteConst.mybookmark} className={`bg-red-600 ${route !== RouteConst.mybookmark ? 'hidden' : ''}`}>
           <FontAwesomeIcon icon={faBookBookmark} style={{ color: '#000000' }} className="bg-red-600 pl-4" />
           <span className="ml-2 bg-red-600 text-white">북마크</span>
         </Link>
-        <Link to={RouteConst.mymission} className=" bg-purple-600">
+        <Link to={RouteConst.mymission} className={`bg-purple-600 ${route !== RouteConst.mymission ? 'hidden' : ''}`}>
           <FontAwesomeIcon icon={faMap} style={{ color: '#000000' }} className="bg-purple-600 pl-4" />
           <span className="ml-2 bg-purple-600 text-white">미션</span>
         </Link>
