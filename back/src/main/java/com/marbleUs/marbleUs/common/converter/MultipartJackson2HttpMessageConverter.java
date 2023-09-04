129bca4f273d0ac330f00ca73f07e5cc20a8d2af
SEB_BE_45_전우현 <tbvjdngus@gmail.com>
2023-09-04 17:21:02 +0900

    "Feat/fix/image CRUD구현 (Converter 추가)#60"

--- back/src/main/java/com/marbleUs/marbleUs/common/converter/MultipartJackson2HttpMessageConverter.java
+++ back/src/main/java/com/marbleUs/marbleUs/common/converter/MultipartJackson2HttpMessageConverter.java
@@ -1,4 +1,4 @@
-package com.marbleUs.marbleUs.converter;
+package com.marbleUs.marbleUs.common.converter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.MediaType;
