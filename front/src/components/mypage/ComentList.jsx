9e8d2f5a9e87a41690264fc6d498adf6e32800f3
toystory23 <ghlduq@naver.com>
2023-09-06 15:52:05 +0900

    #35 myBlog 페이지 동적 구현

--- front/src/components/mypage/ComentList.jsx
+++ front/src/components/mypage/ComentList.jsx
@@ -1,22 +1,24 @@
-import data from '../../pages/mypage/data.json';
+import { useRecoilValue } from 'recoil';
+import Pagenation from './Pagenation';
+import { User } from '../../recoil/mypage';
 
 export default function ComentList() {
   return (
-    <div className="flex flex-col w-[42.5rem] h-[25rem] justify-around bg-white">
-      <div className="flex flex-col h-[17rem]">
+    <div className="flex flex-col w-[50rem] h-[25rem] items-center justify-around bg-white shadow-pageCenter">
+      <div className="flex flex-col w-[42.5rem] h-[17rem]">
         <div className="bg-white pb-2">작성 댓글 목록</div>
         <div className="flex flex-col pt-2 h-[15rem] justify-between">
-          {data.data.map((item) => {
+          {/* {data.myComments.map((item) => {
             return (
               <div>
-                <div key={item.id}>{item.title}</div>
-                <div key={item.id + 4}>{item.createdAt}</div>
+                <div key={item.id}>{item.body}</div>
+                <div key={item.id + 100}>{item.createdAt.slice(0,10)}</div>
               </div>
             );
-          })}
+          })} */}
         </div>
       </div>
-      <div>페이지네이션 구역</div>
+      <Pagenation/>
     </div>
   );
 }
