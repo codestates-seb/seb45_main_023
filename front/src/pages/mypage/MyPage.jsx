5c8b112b227c26c52614c29bb39781c66fcf4ef5
toystory23 <ghlduq@naver.com>
2023-09-06 15:52:39 +0900

    #16 마이페이지 및 수정 페이지 동적 구현

--- front/src/pages/mypage/MyPage.jsx
+++ front/src/pages/mypage/MyPage.jsx
@@ -1,18 +1,55 @@
-import LeftSidebar from '../../components/mypage/LeftSidebar';
-import RightSidebar from '../../components/mypage/RightSidebar';
+import TopSidebar from '../../components/mypage/TopSidebar';
+import BottomSidebar from '../../components/mypage/BottomSidebar';
 import MypageNotice from '../../components/mypage/MypageNotice';
 import UserInfo from '../../components/mypage/UserInfo';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useRecoilState } from 'recoil';
+import { User, userInfo } from '../../recoil/mypage';
 
 export default function MyPage() {
+  const [data, setData] = useRecoilState(User);
+  const [info, setInfo] = useRecoilState(userInfo);
+  const [nickname, setNickname] = useState(data.nickname);
+  const [nationality, setNationality] = useState(data.nationality);
+  const [password, setPassword] = useState('');
+  const [birth, setBirth] = useState(data.birth);
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const data = await axios.get('https://9129-116-126-166-12.ngrok-free.app/members/test@gmail.com', {
+          headers: {
+            'Content-Type': 'application/json',
+            'ngrok-skip-browser-warning': '69420',
+          },
+        });
+        setData(data.data);
+        const { nickname, nationality, password, birth, id } = data.data;
+        setInfo({ nickname, nationality, password, birth, id });
+      } catch (err) {
+        console.log(err);
+      }
+    };
 
+    getData();
+  }, []);
   return (
-    <div className="flex justify-center">
-      <LeftSidebar />
-      <div className="flex flex-col items-center w-[50rem] h-[50rem] mt-[3rem] shadow-xss rounded-[2rem] pb-4 bg-white">
-        <MypageNotice />
-        <UserInfo/>
+    <div className="flex justify-center bookshelf-animation">
+      <TopSidebar />
+      <BottomSidebar />
+      <div className="flex flex-col items-center w-[50rem] h-[50rem] mt-[3rem] shadow-xss rounded-t-[2rem] bg-white ">
+        <MypageNotice nickname={nickname} nationality={nationality} password={password} birth={birth} />
+        <UserInfo
+          setNickname={setNickname}
+          setNationality={setNationality}
+          setPassword={setPassword}
+          setBirth={setBirth}
+        />
+        <div className="bg-white h-[0.5rem] w-[50rem] border-b-[1px] border-gray-300">&nbsp;</div>
+        <div className="bg-white h-[0.5rem] w-[50rem] border-b-[1px] border-gray-300">&nbsp;</div>
+        <div className="bg-white h-[0.5rem] w-[50rem] border-b-[1px] border-gray-300">&nbsp;</div>
+        <div className="bg-white h-[0.5rem] w-[50rem] rounded-b-3xl shadow-xss">&nbsp;</div>
       </div>
-      <RightSidebar />
     </div>
   );
 }
