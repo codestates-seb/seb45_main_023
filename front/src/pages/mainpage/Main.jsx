b078938403294b503e83eb1b08b69edee7d6763c
3kiein <daisy.airdrop_08@icloud.com>
2023-09-07 10:08:19 +0900

    [remove] delete three

--- front/src/pages/mainpage/Main.jsx
+++ front/src/pages/mainpage/Main.jsx
@@ -1,6 +1,3 @@
-import { OrbitControls } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
-
 import PassportIcon from "../../components/PassportIcon";
 
 import {
@@ -20,17 +17,8 @@ import {
 export default function Main() {
 	return (
 		<div className="App">
-			<Canvas>
-				<OrbitControls autoRotate={true} />
-				<mesh>
-					<ambientLight intensity={1} />
-					<directionalLight position={[-1, 0, 1]} intensity={0.5} />
-					<boxGeometry args={[3, 3, 3]} />
-					<meshStandardMaterial attach="material" color={505050} />
-				</mesh>
-			</Canvas>
-			<Button text={"로그아웃"} color={'blue'}/>
-			<Button text={"개인정보수정"} color={'blue'}/>
+			<Button text={"로그아웃"} color={"blue"} />
+			<Button text={"개인정보수정"} color={"blue"} />
 			<GreenButton text={"인증글 쓰러가기"} />
 			<PurpleButton text={"인증글 쓰러가기"} />
 			<NegativeButton text={"글 쓰기"} />
