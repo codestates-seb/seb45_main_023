e0844e759bcc402622e67379153536d780aa4e3a
SEB_BE_45_전우현 <tbvjdngus@gmail.com>
2023-09-12 10:12:27 +0900

    "feat/multi-search 구현"

--- back/src/main/java/com/marbleUs/marbleUs/blog/repository/BlogRepository.java
+++ back/src/main/java/com/marbleUs/marbleUs/blog/repository/BlogRepository.java
@@ -1,6 +1,8 @@
 package com.marbleUs.marbleUs.blog.repository;
 
 import com.marbleUs.marbleUs.blog.entity.Blog;
+import com.marbleUs.marbleUs.member.entity.Member;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -14,6 +16,8 @@ public interface BlogRepository extends JpaRepository<Blog,Long> {
     List<Blog> findByTagsContaining(String tag);
     List<Blog> findByMemberId(Long memberId);
     List<Blog> findByCityId(Long cityId);
-    //Optional<Blog> findByMemberId(long blogId);
 
+
+    @Query("SELECT b FROM Blog b where b.title LIKE %:word% OR b.body LIKE %:word%")
+    List<Blog> findAllByKeywords(String word);
 }
