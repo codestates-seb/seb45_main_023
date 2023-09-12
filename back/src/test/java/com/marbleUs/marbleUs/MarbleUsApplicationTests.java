dc641ebdf82ff2b332f0539ab1c53c8ab801d8a9
SEB_BE_45_전우현 <tbvjdngus@gmail.com>
2023-09-12 17:36:38 +0900

    "feat/fix-2"

--- back/src/test/java/com/marbleUs/marbleUs/MarbleUsApplicationTests.java
+++ back/src/test/java/com/marbleUs/marbleUs/MarbleUsApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("local")
+@ActiveProfiles("deploy")
 class MarbleUsApplicationTests {
 
 	@Test
