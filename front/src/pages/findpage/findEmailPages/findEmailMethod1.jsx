3d26bef506ef87fbc8f6db858c7679c4a383edc2
김우주 <https://github.com/ghvhdh333>
2023-09-14 14:01:39 +0900

    feat/oauth

--- front/src/pages/findpage/findEmailPages/findEmailMethod1.jsx
+++ front/src/pages/findpage/findEmailPages/findEmailMethod1.jsx
@@ -1,9 +1,103 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useRecoilState } from 'recoil';
+import { Link, useNavigate } from "react-router-dom";
+import { emailState, passwordState } from '../../../recoil/logInSignUpState';
 import {FindSubmitButton, ToSmallButton} from "../../../components/Buttons";
 
 export default function FindEmailMethod1 () {
+    const navigate = useNavigate();
+    
+    // 상태관리 정의
+    const [email, setEmail] = useRecoilState(emailState);
+    const [password, setPassword] = useRecoilState(passwordState);
+    const [message, setMessage] = useState('');
+
+    const [errors, setErrors] = useState({});
+    const [isLoading, setIsLoading] = useState(false);
+
+    // email 유효성 검사 조건 : 영어+숫자._-@영어+숫자.-.영어+숫자 (= 일반적인 이메일 형식)
+    const validateEmail = (email) => {
+        const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+        return emailRegex.test(email);
+    };
+
+    // password 유효성 검사 조건 : 영어+숫자+특수문자(@$!%*?&)를 모두 포함하며, 최소 8자 이상 (= 일반적인 비밀번호 형식)
+    const validatePassword = (password) => {
+        const passwordRegex =
+            /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+        return passwordRegex.test(password);
+    };
+
+    const validateForm = () => {
+        const errors = {};
+
+        if (!validateEmail(email)) {
+            errors.email = "올바른 이메일 형식이 아닙니다.";
+        }
+
+        if (!validatePassword(password)) {
+            errors.password =
+                "비밀번호는 영어, 숫자, 특수문자를 모두 포함하고 최소 8자 이상이어야 합니다.";
+        }
+
+        setErrors(errors);
+
+        return Object.keys(errors).length === 0;
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        if (!validateForm()) {
+            return;
+        }
+
+        // 로딩 상태 설정
+        setIsLoading(true);
+
+        // API 요청을 보내기 위한 데이터 준비
+        const requestData = {
+            email,
+            password,
+        };
+
+        try {
+            // 서버 API 호출
+            const response = await axios.post(
+                `${process.env.REACT_APP_SERVER_URL}/엔드포인트/???`,
+                requestData,
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                }
+            );
+
+            console.log(response)
+            console.log(response.data)
+
+            // 로그인 성공 시 기존의 이메일을 받아옴
+            const getEmail = response.data.email;
+
+            // 상태로 이메일 저장
+            setMessage(`찾으시는 이메일은 ${getEmail} 입니다.`);
+
+            alert(message)
+
+            // 로그인페이지로 이동
+            navigate("/login");
+        } catch (error) {
+            // 로딩 상태 해제
+            setIsLoading(false);
+
+            // 로그인 실패 처리
+            setErrors({ serverError: "조건에 맞는 이메일이 없습니다." });
+        }
+    };
+
     return (
         <div className="flex justify-center items-center h-[100vh]">
-            <form className="flex flex-col  items-center w-[30rem] h-[50rem] shadow-xss rounded-[2rem] bg-[#F6F8FA]">
+            <form onSubmit={handleSubmit} className="flex flex-col  items-center w-[30rem] h-[50rem] shadow-xss rounded-[2rem] bg-[#F6F8FA]">
                 <div className='flex justify-between items-center w-[100%] px-[40px] mt-[20px] mb-[100px]'>
                     <span className="font-bold text-black text-[24px]">
                         MarbleUs
@@ -24,23 +118,23 @@ export default function FindEmailMethod1 () {
                 <div className="flex flex-col gap-[30px]">
                     <div className="flex flex-col justify-center">
                         <label htmlFor="email" className="font-medium text-[20px] block mb-[7px]">
-                            입력 1
+                            이메일 / E-mail
                         </label>
-                        <input type="email" name="email" placeholder="아직" className=" w-[100%] h-[50px] bg-white text-[#6C6C6C] text-[18px] border-2 border-[#D7D7D7] rounded-[10px] pl-[14px]">
+                        <input type="email" name="email" id="email" placeholder="email@example.com" value={email} onChange={(event) => setEmail(event.target.value)} className="w-[100%] h-[50px] bg-white text-[#6C6C6C] text-[18px] border-2 border-[#D7D7D7] rounded-[10px] pl-[14px]">
                         </input> 
                     </div>
                     <div className="flex flex-col justify-center">
-                        <label htmlFor="email" className="font-medium text-[20px] block mb-[7px]">
-                            입력 2
+                        <label htmlFor="password" className="font-medium text-[20px] block mb-[7px]">
+                            비밀번호 / Password
                         </label>
-                        <input type="email" name="email" placeholder="기능구현을" className=" w-[100%] h-[50px] bg-white text-[#6C6C6C] text-[18px] border-2 border-[#D7D7D7] rounded-[10px] pl-[14px]">
+                        <input type="password" name="password" id="password" placeholder="••••••••" value={email} onChange={(event) => setPassword(event.target.value)} className="w-[100%] h-[50px] bg-white text-[#6C6C6C] text-[18px] border-2 border-[#D7D7D7] rounded-[10px] pl-[14px]">
                         </input> 
                     </div>
                     <div className="flex flex-col justify-center">
                         <label htmlFor="email" className="font-medium text-[20px] block mb-[7px]">
                             입력 3
                         </label>
-                        <input type="email" name="email" placeholder="못하였습니다." className=" w-[100%] h-[50px] bg-white text-[#6C6C6C] text-[18px] border-2 border-[#D7D7D7] rounded-[10px] pl-[14px]">
+                        <input type="email" name="email" placeholder="못하였습니다." className="w-[100%] h-[50px] bg-white text-[#6C6C6C] text-[18px] border-2 border-[#D7D7D7] rounded-[10px] pl-[14px]">
                         </input> 
                     </div>
                     <FindSubmitButton text='Submit' />
