039f383e373705b0691690e203ff563c49e72ef8
toystory23 <ghlduq@naver.com>
2023-09-17 23:40:02 +0900

    페이지네이션 props 설정

--- front/src/components/mypage/BlogPagination.jsx
+++ front/src/components/mypage/BlogPagination.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import Pagination from 'react-js-pagination';
 import './Pagination.css'
 
-export default function BlogPagenation() {
+export default function BlogPagenation({itemsCountPerPage,totalItemsCount,renderItemCount}) {
   const [page, setPage] = useState(1);
 
   const handlePageChange = (page) => {
@@ -13,9 +13,9 @@ export default function BlogPagenation() {
       <div className='flex justify-center'>
         <Pagination
           activePage={page}
-          itemsCountPerPage={4}
-          totalItemsCount={10}
-          pageRangeDisplayed={4}
+          itemsCountPerPage={itemsCountPerPage} //페이지 당 카운트할 갯수
+          totalItemsCount={totalItemsCount} // 컨텐츠 총 갯수
+          pageRangeDisplayed={renderItemCount} // 보여줄 페이지 갯수
           prevPageText={'<'}
           nextPageText={'>'}
           onChange={handlePageChange}
